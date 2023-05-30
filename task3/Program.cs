4ead8a5f07bfd0f8c104103b07e16ccd27dfcc39
Kate <kataroslakova10@gmail.com>
2023-05-30 17:18:53 +0300

    день недели

---
+++ task3/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите номер дня недели");
+int x = int.Parse(Console.ReadLine()!);
+if (x == 1 || x == 2 || x == 3 || x == 4 || x == 5)
+System.Console.WriteLine("День под эти номером не выходной");
+else if (x > 7)
+System.Console.WriteLine("Такого дня недели нет");
+else
+System.Console.WriteLine("День под этим номером выходной");
